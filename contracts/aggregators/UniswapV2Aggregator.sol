617ed41f0b1d761bdb2370839ba0eb8801e830cc
murat <murat@murats-mbp.local>
2022-10-03 17:07:14 +0300

    @feat: complete constructor

--- contracts/aggregators/UniswapV2Aggregator.sol
+++ contracts/aggregators/UniswapV2Aggregator.sol
@@ -13,6 +13,7 @@ import "../interfaces/IPriceOracle.sol";
 contract UnipswapV2Aggregator {
     using SafeMath for uint256;
 
+    uint256 public constant BONE = 10**18;
     IUniswapV2Pair public immutable pair;
     IPriceOracle immutable priceOracle;
     address[] public tokens;
@@ -26,7 +27,11 @@ contract UnipswapV2Aggregator {
         uint256 _maxPriceDeviation,
         bool[] memory _isPeggedToEth,
         uint8[] memory _decimals) {
-        
+        require(_isPeggedToEth.length == 2, "ERR_INVALID_PEGGED_LENGTH");
+        require(_decimals.length == 2, "ERR_INVALID_DECIMALS_LENGTH");
+        require(address(_priceOracle) != address(0), "ERR_INVALID_PRICE_ORACLE");
+        require(_maxPriceDeviation < BONE, "ERR_INVALID_MAX_PRICE_DEVIATION");
+
         pair = _pair;
         priceOracle = _priceOracle;
         maxPriceDeviation = _maxPriceDeviation;
